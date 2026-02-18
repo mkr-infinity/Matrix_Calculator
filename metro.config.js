95a47f870b00841f08651d307cb8e758a71c80fa
Kaif <125804924+mkr-infinity@users.noreply.github.com>
2026-02-18 18:02:21 +0530

    Update metro.config.js to use Expo Metro Config

--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,5 @@
-import { getDefaultConfig } from "expo/metro-config";
+const { getDefaultConfig } = require("expo/metro-config");
 
 const config = getDefaultConfig(__dirname);
 
-export default config;
+module.exports = config;
